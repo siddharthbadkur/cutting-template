8e7642bed0da3e86191151a520adbfff9c69ed51
siddharth <badkursiddharth282@gmail.com>
2024-07-02 23:53:52 +0530

    commit of template

---
+++ project/app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path 
+from .views import *
+
+urlpatterns =[
+    path('home/',home,name='home'),
+    path('about/',about,name='about'),
+    path('contact/',contact,name='contact')
+
+]
